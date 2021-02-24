01cc61fb2de12424fa6cb5719c570d5d96581c58
iqfan123 <boongiqfan@gmail.com>
2021-02-24 15:06:25 +0700

    commit Khanif

--- resources/views/layout/v_template.blade.php
+++ resources/views/layout/v_template.blade.php
@@ -35,11 +35,7 @@
 
   <header class="main-header">
     <!-- Logo -->
-<<<<<<< HEAD
-    <a href="../../index2.html" class="logo">
-=======
     <a href="home" class="logo">
->>>>>>> khanif453
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>A</b>LT</span>
       <!-- logo for regular state and mobile devices -->
@@ -213,7 +209,3 @@
 </script>
 </body>
 </html>
-<<<<<<< HEAD
- 
-=======
->>>>>>> khanif453
